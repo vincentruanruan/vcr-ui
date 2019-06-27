1ff3a22f15cdbb00352e920e48a5d5464eea6bd7
vincentruan <ruan81@vip.qq.com>
2019-06-27 11:25:50 +0800

    更新忽略文件

---
+++ packages/input/main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <input type="text" value="iiiiiiiiiiii">
+    </div>
+</template>
+
+
+<script>
+export default {
+    name : 'vc-input'
+}
+</script>
+
+
+<style lang="sass">
+    
+</style>
---
+++ packages/textarea/main.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <textarea name="" id="" cols="30" rows="10">
+        asdsadsad</textarea>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name : 'vc-textarea'
+}
+</script>
+
+
+<style lang="sass">
+    
+</style>
